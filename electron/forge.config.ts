fce4537697283500d055b3986f501ed354a0d9f0
Exponential-Workload <cc@aaathats3as.com>
2023-03-08 18:57:22 +0000

    chore: forge shenanigans

--- electron/forge.config.ts
+++ electron/forge.config.ts
@@ -1,17 +1,29 @@
+// @ts-nocheck
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
 import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerDMG } from '@electron-forge/maker-dmg'
+import { MakerDMG } from '@electron-forge/maker-dmg';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
 
 const config: ForgeConfig = {
-  packagerConfig: {},
+  packagerConfig: {
+    executableName: 'AnonApp'
+  },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin', 'linux']), new MakerDeb({}), new MakerDMG({ format: 'ULFO' })],
+  makers: [
+    new MakerSquirrel({}),
+    new MakerZIP({}, ['darwin', 'linux']),
+    new MakerDeb({
+      options: {
+        bin: 'AnonApp'
+      }
+    }),
+    new MakerDMG({ format: 'ULFO' }),
+  ],
   plugins: [
     new WebpackPlugin({
       mainConfig,
